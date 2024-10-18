1d3c92f4e89e08153420c480ea9afd597da7fca2
Adrian-afk-source <madab1234567@gmail.com>
2024-10-18 17:02:43 -0500

    Added partial assignment

---
+++ Team Assignment EC AG.py
@@ -0,0 +1,17 @@
+# Emmanuel Cruz Adrian Garcia
+
+print("1. Wake up")
+print("2. Eat Breakfast")
+print("3. Learn Python")
+print("4. Working on homework")
+print("5. Going on a run")
+print("6. Eat dinner")
+print("7. Go to bed")
+print("0. Exit")
+
+w = input("Please enter your answer?:")
+if w == "1":
+    print("Have something to eat") 
+
+
+
